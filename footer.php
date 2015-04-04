dbddb3a8f974ee220e4ae3bd9c6f545eaaeaee88
soxey6 <soxey6@gmail.com>
2015-04-04 16:12:38 +1100

    Quick update

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spanish
+ * Date: 4/04/15
+ * Time: 3:14 PM
+ */
+?>
+<!--
+    Look, it's an unused footer. YAY.
+-->
--- header.php
+++ header.php
@@ -5,6 +5,9 @@
  * Date: 4/04/15
  * Time: 2:44 PM
  */
+/*
+ * START: Includes and setup
+ */
 // Require the main.php which will include all the libraries and other files needed.
 require_once("inc/main.php");
 // SQL to retrieve the current theme based on the setting found in the settings table.
@@ -16,3 +19,25 @@ $loader = new Twig_Loader_Filesystem('content/themes/'.$theme["setting_value"]."
 $twig = new Twig_Environment($loader, array(
     'cache' => 'content/cache/',
 ));
+
+/*
+ * END: Includes and setup
+ */
+
+/*
+ * START: Theme
+ */
+/* This chunk of code does the following
+ * Creates an empty array to replace variables with from the template
+ * Queries the DB for any variables
+ * Adds them to the array.
+ * Parses the variable through the hookmanager where plugins may add to it.
+ */
+$templateReplacementArray = array();
+$result=$sql->query("SELECT * FROM `tempalte_variables`");
+while($row = $result->fetch_assoc()) {
+    $templateReplacementArray[$row["to_replace"]]=$row["replacement"];
+}
+// The hook manager will return the modified $templateReplacementArray.
+$templateReplacementArray=$hookManager->triggerHook("template_variables",array($templateReplacementArray));
+echo $twig->render('header.html', $templateReplacementArray);
